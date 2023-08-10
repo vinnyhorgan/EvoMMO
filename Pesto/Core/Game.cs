2f9ab165ea474c9a72cb9d33b1abd480b7dce41e
Vinny Horgan <vinnyhorgan@gmail.com>
2023-08-10 11:55:44 +0200

    first commit

---
+++ Pesto/Core/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace Pesto.Core
+{
+    public class Game
+    {
+        private static Game _instance;
+
+        private Game()
+        {
+
+        }
+
+        public static Game Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Game();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
